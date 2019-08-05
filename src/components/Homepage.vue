ee542cea3c7c752ee70fd9a75ba648f95a9c3089
baboucarr <baboucarrd43@gmail.com>
2019-08-05 16:33:44 +0000

    modified the with of the container

--- src/components/Homepage.vue
+++ src/components/Homepage.vue
@@ -35,7 +35,7 @@
 
         <div class="services__card spacer-1">
           <img class="card__image" src="/../images/icone-sms-long-petit.png" alt="sms" />
-          <a class="card__title" href="#">SMS Mailing  (envoi de mailing SMS)</a>
+          <a class="card__title" href="#">SMS Mailing  <br>  (envoi de mailing SMS)</a>
           <p
             class="card__description"
           >Envoyez une mailing SMS à autant de destinataires que vous le souhaitez. Annoncer un événement, une promotion, un nouveau produit, etc.... à plusieurs contacts est réalisable en quelques clics !</p>
@@ -128,6 +128,11 @@ export default {
   color: $color-white;
   font-weight: bold;
   font-size: 0.7rem;
+  cursor: pointer;
+
+  &:hover{
+    background: #fb057c;
+  }
 }
 
 .revolution__btn {
