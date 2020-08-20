cc5128e1aa545e420c87e666469a8edcf8bff3f3
baboucarr <baboucarrd43@gmail.com>
2020-08-20 17:03:09 +0800

    add component

--- src/components/Sondage.vue
+++ src/components/Sondage.vue
@@ -42,7 +42,180 @@
         obtenues et comment obtenir les statistiques d’envoi et de réception de
         votre enquête par SMS !
       </p>
+      <div class="cards spacer-1">
+        <div class="card">
+          <img
+            class="card__image"
+            src="/../images/sondage/Imp-cont.png"
+            alt=""
+            srcset=""
+          />
+          <div class="card__details">
+            <h3>Import des contacts</h3>
+            <p>
+              Importer votre base de données avec les numéros de téléphones
+              portables de vos destinataires. Le fichier peut intégrer des
+              variables (nom, prénom…) que vous pourrez intégrer dans le
+              message.
+            </p>
+          </div>
+        </div>
+        <div class="card">
+          <img
+            class="card__image"
+            src="/../images/sondage/Creation-sondage.png"
+            alt=""
+          />
+          <div class="card__details">
+            <h3>Création du sondage</h3>
+            <p>
+              Créer votre sondage directement sur la plateforme et intégrez des
+              questions personnalisées suivant les réponses émises par vos
+              destinataires. Ainsi vous bénéficierez d’une personnalisation de
+              vos questions.
+            </p>
+          </div>
+        </div>
+        <div class="card">
+          <img
+            class="card__image"
+            src="/../images/sondage/Envoi-sondage.png"
+            alt=""
+          />
+          <div class="card__details">
+            <h3>Envoi du sondage</h3>
+            <p>
+              Envoyer votre campagne et laissez la plateforme effectuer la
+              gestion des envois et des réponses. L’ensemble des messages seront
+              envoyés en quelques minutes, la campagne dure 48 heures.
+            </p>
+          </div>
+        </div>
+      </div>
+      <h2 class="center__text">Frais d'envoi du sondage SMS tout inclus !</h2>
+      <p class="center__text price__text">29,62 CFA/SMS</p>
+      <h2 class="center__text">
+        Fonctionnalités disponibles avec le sondage par SMS
+      </h2>
+      <div class="contact__us">
+        <p>
+          Pour en savoir plus sur l’utilisation du sondage par SMS ?)
+          (CONTACTEZ-NOUS !
+        </p>
+        <button class="btn">CONTACTEZ-NOUS</button>
+      </div>
+      <div class="cards spacer-1">
+        <div class="card">
+          <img
+            class="card__image"
+            src="/../images/sondage/Recup-result.png"
+            alt=""
+          />
+          <div class="card__details">
+            <h3>Récupération des résultats</h3>
+            <p>
+              Une fois la campagne terminée, les SMS seront analysés et un
+              rapport sera généré automatiquement sur la plateforme. Un tableau
+              récapitulatif des résultats de votre enquête sera aussi
+              disponible. Vous pouvez également télécharger l’ensemble des
+              réponses à vos questions ouvertes au format Excel ou PDF.
+            </p>
+          </div>
+        </div>
+        <div class="card">
+          <img
+            class="card__image"
+            src="/../images/sondage/Creation-sondage.png"
+            alt=""
+          />
+          <div class="card__details">
+            <h3>Formats de réponses</h3>
+            <p>
+              Choisissez pour chacune de vos questions le format de réponse
+              adapté parmi ceux proposés sur la plateforme : note chiffrée,
+              réponses ouvertes, réponses « oui/non » ou réponses multiples !
+            </p>
+          </div>
+        </div>
+        <div class="card">
+          <img
+            class="card__image"
+            src="/../images/sondage/icone-relance.png"
+            alt=""
+          />
+          <div class="card__details">
+            <h3>Des relances adaptées</h3>
+            <p>
+              N’hésitez pas à relancer les personnes n’étant pas allé au bout de
+              votre sondage sms. Avec une relance personnalisée, les sondés
+              répondront à votre sondage et vous obtiendrez un maximum de
+              réponses.
+            </p>
+          </div>
+        </div>
+        <div class="card">
+          <img
+            class="card__image"
+            src="/../images/sondage/icone-remerci.png"
+            alt=""
+          />
+          <div class="card__details">
+            <h3>SMS de clôture</h3>
+            <p>
+              Une fois la dernière question de votre sondage envoyée, vous
+              pouvez le clôturer avec un sms personnalisé. Celui-ci permet de
+              remercier les personnes sondées de leur participation.
+            </p>
+          </div>
+        </div>
+        <div class="card">
+          <img
+            class="card__image"
+            src="/../images/sondage/icone-programme.png"
+            alt=""
+          />
+          <div class="card__details">
+            <h3>Programmation du sondage</h3>
+            <p>
+              C’est à vous seul de décider du moment d’envoi de votre sondage
+              par sms. Vous pouvez l’envoyer en instantané ou bien le programmer
+              au jour et à l’heure que vous souhaitez !
+            </p>
+          </div>
+        </div>
+        <div class="card">
+          <img
+            class="card__image"
+            src="/../images/sondage/icone-export.png"
+            alt=""
+          />
+          <div class="card__details">
+            <h3>Exportation des réponses</h3>
+            <p>
+              Vous recevez les réponses à votre sondage sms sur la plateforme ou
+              sur votre boîte mail selon votre préférence. Vous pouvez aussi les
+              exporter au format Excel pour les conserver sur votre ordinateur.
+            </p>
+          </div>
+        </div>
+        <div class="card">
+          <img
+            class="card__image"
+            src="/../images/sondage/icone-statistic.png"
+            alt=""
+          />
+          <div class="card__details">
+            <h3>Analyse des statistiques</h3>
+            <p>
+              Suivez en temps réel les statistiques de réception et d’envoi de
+              votre sondage par SMS ! Optimisez votre analyse en téléchargeant
+              les statistiques au format PDF.
+            </p>
+          </div>
+        </div>
+      </div>
     </section>
+    <Footer/>
   </div>
 </template>
 
@@ -57,4 +230,29 @@ export default {
 };
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+@import "../assets/base.scss";
+.price__text {
+  background-image: url("/../images/sondage/price-tarifs.png");
+  width: 250px;
+  height: 250px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-repeat: no-repeat;
+  color: #fff;
+  margin: 0 auto;
+  font-weight: bold;
+}
+.contact__us {
+  background: $color-tertiary;
+  padding: 4rem;
+  text-align: center;
+  color: $color-white;
+}
+.card{
+    border-radius: 0.25rem;
+    
+    box-shadow: 0 20px 40px -14px rgba(0, 0, 0, 0.25);
+}
+</style>
