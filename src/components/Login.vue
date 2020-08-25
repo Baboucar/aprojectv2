2aff2729604df7fbf3be769892672e5f3b8a0d92
baboucarr <baboucarrd43@gmail.com>
2020-08-25 15:07:53 +0800

    modified forms

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -3,23 +3,28 @@
     <Navigation />
 
     <section class="top__container section__containe">
-      <form action=""  class="form">
-            <h1 class="center__text heading">CONNEXION</h1>
+      <form action="" class="form">
+        <h1 class="center__text heading">CONNEXION</h1>
 
         <div class="form__group">
-          <label for="" class="">Votre E-mail:</label>
           <input type="email" name="" id="" placeholder="Vote Email" />
         </div>
         <div class="form__group">
-          <label for="">Votre mot de passe:</label>
-          <input type="password" name="" id="" placeholder="Votre mot de passe"/>
+          <input
+            type="password"
+            name=""
+            id=""
+            placeholder="Votre mot de passe"
+          />
         </div>
-        <button class="form__btn">Connexion</button>
-        <p class="center__text title">mot de passe oublié
-</p>
+        <div class="center__button">
+          <button class="form__btn">Connexion</button>
+        </div>
+
+        <p class="center__text title">mot de passe oublié</p>
       </form>
     </section>
-    <Footer/>
+    <Footer />
   </div>
 </template>
 
@@ -35,35 +40,13 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-@import  "../assets/base.scss";
-label {
-  display: block;
-  font-weight: bold;
-  margin-top: 1rem;
-}
-.form__group {
-    width: 90%;
-    margin: 0 auto;
-}
 
-    .form{
-   
-  border-radius: 0.25rem;
-  box-shadow: 0 20px 40px -14px rgba(0, 0, 0, 0.25);
-  margin: 2rem auto;
-  padding: 4rem;
-  
-}
 
-:-moz-placeholder{
-  padding: 1rem;
-}
 @import "../assets/base.scss";
 input[type="password"],
 input[type="email"] {
- width: 100%;
+  width: 90%;
   display: block;
-
   box-sizing: border-box;
   border: none;
   color: #555;
@@ -74,30 +57,44 @@ input[type="email"] {
   border-radius: 4px;
   -webkit-box-shadow: inset 0 1px 1px rgba(0, 0, 0, 0.075);
   box-shadow: inset 0 1px 1px rgba(0, 0, 0, 0.075);
-  margin-top: 1rem;
+  margin: 1rem auto;
 }
 
+.text {
+  height: 50px;
+  margin-top: 1rem;
+  width: 90%;
+  margin: 1rem auto;
+}
+::placeholder {
+  padding-left: 1rem;
+}
+
+.center__button {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
 .form__btn {
-  
   width: 50%;
-  display: block;
-  margin: 2rem  auto;
+  margin-bottom: 1rem;
   padding: 1rem;
   background: $color-tertiary;
   border: none;
   box-sizing: border-box;
   color: $color-white;
   font-weight: bold;
-  margin-bottom: 2rem;
+
   cursor: pointer;
   &:hover {
     background: rgb(32, 32, 133);
   }
 }
 @media (min-width: 64rem) {
-  .form{
-   width: 50%;
-   margin: 4rem auto;
+  .form {
+    width: 50%;
+    margin: 4rem auto;
+    padding: 4rem;
   }
 }
 </style>
--- src/components/Signup.vue
+++ src/components/Signup.vue
@@ -10,14 +10,14 @@
           sera envoyé par e-mail.</p>
 
           <div>
-          <label for="">Email</label>
           <input type="email" name="" id="" placeholder="Votre email">
           </div>
           <div>
              <vue-tel-input v-model="phone" class="text"> </vue-tel-input>
           </div>
+                  <div class="center__button">
                   <button class="form__btn">Valider</button>
-
+                   </div>
       </form>
     </section>
     <Footer/>
@@ -48,25 +48,23 @@ label {
   margin-top: 1rem;
 }
 .form__group {
-    width: 90%;
+    width: 95%;
     margin: 0 auto;
 }
 
     .form{
-   
   border-radius: 0.25rem;
   box-shadow: 0 20px 40px -14px rgba(0, 0, 0, 0.25);
   margin: 2rem auto;
-  padding: 4rem;
+  
   
 }
 
 @import "../assets/base.scss";
 input[type="password"],
 input[type="email"]{
- width: 100%;
+ width: 90%;
   display: block;
-
   box-sizing: border-box;
   border: none;
   color: #555;
@@ -77,29 +75,34 @@ input[type="email"]{
   border-radius: 4px;
   -webkit-box-shadow: inset 0 1px 1px rgba(0, 0, 0, 0.075);
   box-shadow: inset 0 1px 1px rgba(0, 0, 0, 0.075);
-  margin-top: 1rem;
+  margin: 0 auto;
 }
 
 .text{
     height: 50px;
     margin-top: 1rem;
+    width: 90%;
+    margin: 1rem auto;
 }
 ::placeholder{
     padding-left: 1rem;
 }
 
+.center__button{
+   display: flex;
+   align-items: center;
+   justify-content: center;
+}
 .form__btn {
-  
   width: 50%;
-  display: block;
-  margin: 2rem  auto;
+  margin-bottom: 1rem;
   padding: 1rem;
   background: $color-tertiary;
   border: none;
   box-sizing: border-box;
   color: $color-white;
   font-weight: bold;
-  margin-bottom: 2rem;
+ 
   cursor: pointer;
   &:hover {
     background: rgb(32, 32, 133);
@@ -109,6 +112,7 @@ input[type="email"]{
   .form{
    width: 50%;
    margin: 4rem auto;
+     padding: 4rem;
   }
 }
 </style>
